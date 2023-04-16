375b788ab33b8cb92456f63e652dca1e15a73d21
Mahmoud <mellebda@student.eelu.edu.eg>
2023-04-16 10:46:46 +0200

    create MVVM folders done

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const EcommerceApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class EcommerceApp extends StatelessWidget {
+  const EcommerceApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(),
-        backgroundColor: Colors.blue,
-      ),
+      debugShowCheckedModeBanner: false,
+    home: SplashView(),
     );
   }
 }
